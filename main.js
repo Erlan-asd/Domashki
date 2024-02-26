d4390ba428e6c913c5153261e2140adb98a5b98a
Erlan <vipoffacc@gmail.com>
2024-02-26 19:12:57 +0600

    init

---
+++ main.js
@@ -0,0 +1,64 @@
+// 1
+
+// let number = prompt("Ведите число");
+
+// if (number % 2 === 0) {
+//     alert("Число четное");
+// } else {
+//     alert("Число нечетное");
+// }
+
+// 2
+
+// let number2 = prompt("Ведите число");
+
+// if (number2>0) {
+//     alert("Число положительное");
+// } else if (number2==0) {
+//     alert("Число равно нулю")
+// } else {
+//     alert("Число отрицательное")
+// }
+
+// 3
+
+// let number3 = prompt("Ведите число");
+
+// if (number3>0) {
+//     alert("1");
+// } else if (number3==0) {
+//     alert("0")
+// } else {
+//     alert("-1")
+// }
+
+// 4
+
+// let a = 10;
+// let b = 5;
+
+// let math = prompt("Введите арифметический оператор ( +, -, *, %)");
+// switch(math){
+//     case "+":
+//         console.log(a+b);
+//         break;
+       
+//     case "-":
+//         console.log(a-b);
+//         break;
+        
+//     case "*":
+//         console.log(a*b);
+//         break;
+       
+//     case "%":
+//         console.log(a%b);
+//         break;
+      
+// }
+
+// 5
+
+
+// let result ;
+// a+b<4 ? result = "Мало" : result = "Много"
